9172df65d5bbdd273ced9f66bdb8e80975bdcf55
DZhivkovv <dobromirzhivkov01@gmail.com>
2022-12-06 20:19:19 +0200

    Wrap all variables and function inside of an outer function called play.

--- script.js
+++ script.js
@@ -1,173 +1,179 @@
-/*Containers*/
-const gameContainer = document.getElementById('game-container');
-const displayContainer = document.getElementById('display-container');
-const display = document.getElementById('display');
-const scoreContainer = document.getElementById('score-container');
-const resultContainer = document.getElementById('result-container');
+const play = () => {
+    /*Containers*/
+    const gameContainer = document.getElementById('game-container');
+    const displayContainer = document.getElementById('display-container');
+    const display = document.getElementById('display');
+    const scoreContainer = document.getElementById('score-container');
+    const resultContainer = document.getElementById('result-container');
 
-/*Buttons */
-const choices = document.querySelectorAll('.choice');
-const rockButton = document.getElementById('rock-btn');
-rockButton.addEventListener('click', ()=>{
-    playRound('Rock', rounds);
-})
-
-const paperButton = document.getElementById('paper-btn');
-paperButton.addEventListener('click', ()=>{
-    playRound('Paper', rounds);
-})
-
-const scissorsButton = document.getElementById('scissors-btn');
-scissorsButton.addEventListener('click', ()=>{
-    playRound('Scissors', rounds);
-})
-
-const startButton = document.getElementById('start-btn');
-startButton.onclick = () => {
-    startGame();
-}
-
-const restartButton = document.getElementById('restart-btn');
-restartButton.addEventListener('click', ()=>{
-    restartGame();
-})
-
-
-/*Properties*/
-let rounds = 5;
-let playerPoints = 0;
-let computerPoints = 0;
-let roundWinner = '';
-let gameWinner = '';
-
-const textEmoji = {
-    Rock: '✊',
-    Paper: '🤚',
-    Scissors: '✌️',
-}
-
-
-
-function cleanBoard(){
-    scoreContainer.innerHTML = '';
-    display.innerHTML = '';
-    resultContainer.innerHTML = '';
-    
-    rockButton.classList.add('hidden');
-    paperButton.classList.add('hidden');
-    scissorsButton.classList.add('hidden');
-}
-
-function startGame(){
-    cleanBoard();
-    display.innerHTML = `<p id="start-emojis">${textEmoji.Rock} ${textEmoji.Paper} ${textEmoji.Scissors} <br><span>Rock, Paper, Scissors</span></p>`
-
-    startButton.addEventListener('click', ()=>{
-        rockButton.classList.remove('hidden');
-        paperButton.classList.remove('hidden');
-        scissorsButton.classList.remove('hidden');
-        startButton.classList.add('hidden');
-        
-        display.innerHTML = '';
+    /*Buttons */
+    const choices = document.querySelectorAll('.choice');
+    const rockButton = document.getElementById('rock-btn');
+    rockButton.addEventListener('click', ()=>{
+        playRound('Rock', rounds);
     })
-}
 
-function getComputerChoice(){
-    /*Generates a random number netween 0 and 2 which will determine the computer's choice between rock, paper and scissors*/
-    const choiceIndex = Math.floor(Math.random()*3)
-    return choices[choiceIndex].innerText;
-}
+    const paperButton = document.getElementById('paper-btn');
+    paperButton.addEventListener('click', ()=>{
+        playRound('Paper', rounds);
+    })
 
+    const scissorsButton = document.getElementById('scissors-btn');
+    scissorsButton.addEventListener('click', ()=>{
+        playRound('Scissors', rounds);
+    })
 
-function appendElements(playerChoice, computerChoice, winner){
-    if(winner === 'Player'){
-        scoreContainer.innerHTML = `<p><span>You: ${playerPoints} - Computer: ${computerPoints}</span></p>`
-        resultContainer.innerHTML= '<p>Point for you!</p>';
-        display.innerHTML = `<p id="comparison">${textEmoji[playerChoice]} : ${textEmoji[computerChoice]}</p>`
+    const startButton = document.getElementById('start-btn');
+    startButton.onclick = () => {
+        startGame();
     }
-    else if (winner === 'Computer'){
-        scoreContainer.innerHTML = `<p><span>You: ${playerPoints} - Computer: ${computerPoints}</span></p>`
-        resultContainer.innerHTML = '<p>Point for the computer!</p>';
-        display.innerHTML = `<p id="comparison">${textEmoji[playerChoice]} : ${textEmoji[computerChoice]}</p>`
 
+    const restartButton = document.getElementById('restart-btn');
+    restartButton.addEventListener('click', ()=>{
+        restartGame();
+    })
+
+
+    /*Properties*/
+    let rounds = 5;
+    let playerPoints = 0;
+    let computerPoints = 0;
+    let roundWinner = '';
+    let gameWinner = '';
+
+    const textEmoji = {
+        Rock: '✊',
+        Paper: '🤚',
+        Scissors: '✌️',
     }
-    else if(winner === 'None'){
-        scoreContainer.innerHTML = `<p><span>You: ${playerPoints} - Computer: ${computerPoints}</span></p>`
-        resultContainer.innerHTML = '<p>Tie!</p>';
-        display.innerHTML = `<p id="comparison">${textEmoji[playerChoice]} : ${textEmoji[computerChoice]}</p>`
+
+
+
+    function cleanBoard(){
+        scoreContainer.innerHTML = '';
+        display.innerHTML = '';
+        resultContainer.innerHTML = '';
         
+        rockButton.classList.add('hidden');
+        paperButton.classList.add('hidden');
+        scissorsButton.classList.add('hidden');
     }
-}
 
-function playRound(playerChoice){
-    const computerChoice = getComputerChoice();
-    
-    if(playerChoice === computerChoice){
-        roundWinner = 'None';
-        appendElements(playerChoice, computerChoice, 'None');
+    function startGame(){
+        cleanBoard();
+        display.innerHTML = `<p id="start-emojis">${textEmoji.Rock} ${textEmoji.Paper} ${textEmoji.Scissors} <br><span>Rock, Paper, Scissors</span></p>`
+
+        startButton.addEventListener('click', ()=>{
+            rockButton.classList.remove('hidden');
+            paperButton.classList.remove('hidden');
+            scissorsButton.classList.remove('hidden');
+            startButton.classList.add('hidden');
+            
+            display.innerHTML = '';
+        })
     }
-    else if(
-        (playerChoice === 'Rock' && computerChoice === 'Scissors') ||
-        (playerChoice === 'Paper' && computerChoice === 'Rock') ||
-        (playerChoice === 'Scissors' && computerChoice === 'Paper') 
-    ){
-        playerPoints += 1;
-        roundWinner = 'Player';
 
-        if(playerPoints === 5 || computerPoints === 5){
-            endGame()
-            return 'Game Over';
-        } else{
-            appendElements(playerChoice, computerChoice, roundWinner);
+    function getComputerChoice(){
+        /*Generates a random number netween 0 and 2 which will determine the computer's choice between rock, paper and scissors*/
+        const choiceIndex = Math.floor(Math.random()*3)
+        return choices[choiceIndex].innerText;
+    }
+
+
+    function appendElements(playerChoice, computerChoice, winner){
+        if(winner === 'Player'){
+            scoreContainer.innerHTML = `<p><span>You: ${playerPoints} - Computer: ${computerPoints}</span></p>`
+            resultContainer.innerHTML= '<p>Point for you!</p>';
+            display.innerHTML = `<p id="comparison">${textEmoji[playerChoice]} : ${textEmoji[computerChoice]}</p>`
+        }
+        else if (winner === 'Computer'){
+            scoreContainer.innerHTML = `<p><span>You: ${playerPoints} - Computer: ${computerPoints}</span></p>`
+            resultContainer.innerHTML = '<p>Point for the computer!</p>';
+            display.innerHTML = `<p id="comparison">${textEmoji[playerChoice]} : ${textEmoji[computerChoice]}</p>`
+
+        }
+        else if(winner === 'None'){
+            scoreContainer.innerHTML = `<p><span>You: ${playerPoints} - Computer: ${computerPoints}</span></p>`
+            resultContainer.innerHTML = '<p>Tie!</p>';
+            display.innerHTML = `<p id="comparison">${textEmoji[playerChoice]} : ${textEmoji[computerChoice]}</p>`
+            
         }
     }
-    else if (
-        (playerChoice === 'Rock' && computerChoice === 'Paper') ||
-        (playerChoice === 'Paper' && computerChoice === 'Scissors') ||
-        (playerChoice === 'Scissors' && computerChoice === 'Rock') 
-    ){
-        computerPoints += 1;
-        roundWinner = 'Computer';
 
-        if(playerPoints >= 5 || computerPoints >= 5){
-            endGame()
-            return 'Game Over';
-        } else{
-            appendElements(playerChoice, computerChoice, roundWinner);
+    function playRound(playerChoice){
+        const computerChoice = getComputerChoice();
+        
+        if(playerChoice === computerChoice){
+            roundWinner = 'None';
+            appendElements(playerChoice, computerChoice, 'None');
+        }
+        else if(
+            (playerChoice === 'Rock' && computerChoice === 'Scissors') ||
+            (playerChoice === 'Paper' && computerChoice === 'Rock') ||
+            (playerChoice === 'Scissors' && computerChoice === 'Paper') 
+        ){
+            playerPoints += 1;
+            roundWinner = 'Player';
+
+            if(playerPoints === 5 || computerPoints === 5){
+                endGame()
+                return 'Game Over';
+            } else{
+                appendElements(playerChoice, computerChoice, roundWinner);
+            }
+        }
+        else if (
+            (playerChoice === 'Rock' && computerChoice === 'Paper') ||
+            (playerChoice === 'Paper' && computerChoice === 'Scissors') ||
+            (playerChoice === 'Scissors' && computerChoice === 'Rock') 
+        ){
+            computerPoints += 1;
+            roundWinner = 'Computer';
+
+            if(playerPoints >= 5 || computerPoints >= 5){
+                endGame()
+                return 'Game Over';
+            } else{
+                appendElements(playerChoice, computerChoice, roundWinner);
+            }
         }
     }
-}
 
-function getGameWinner (){
-    if(playerPoints > computerPoints){
-        gameWinner = 'You';
-        return gameWinner
+    function getGameWinner (){
+        if(playerPoints > computerPoints){
+            gameWinner = 'You';
+            return gameWinner
+        }
+        else if (computerPoints > playerPoints){
+            gameWinner = 'Computer';
+            return gameWinner
+        }   
     }
-    else if (computerPoints > playerPoints){
-        gameWinner = 'Computer';
-        return gameWinner
-    }   
-}
 
-function endGame(){
-    cleanBoard();
-    const winner = getGameWinner();
-    if (winner === 'You'){
-        display.innerHTML = "<p class='winner-paragraph' id='winner-paragraph-you'>You win!</p>"
-    } else if(winner === 'Computer'){
-        display.innerHTML = "<p class = 'winner-paragraph' id='winner-paragraph-computer'>Computer <br> wins!</p>"
-    }   
-    restartButton.classList.remove('hidden');
-}
+    function endGame(){
+        cleanBoard();
+        const winner = getGameWinner();
+        if (winner === 'You'){
+            display.innerHTML = "<p class='winner-paragraph' id='winner-paragraph-you'>You win!</p>"
+        } else if(winner === 'Computer'){
+            display.innerHTML = "<p class = 'winner-paragraph' id='winner-paragraph-computer'>Computer <br> wins!</p>"
+        }   
+        restartButton.classList.remove('hidden');
+    }
 
-function restartGame(){
-    /*Resets the points */
-    playerPoints = 0;
-    computerPoints = 0;
+    function restartGame(){
+        /*Resets the points */
+        playerPoints = 0;
+        computerPoints = 0;
 
-    /*Hides the restart button and shows the 'start game' button */
-    restartButton.classList.add('hidden');
-    startButton.classList.remove('hidden')
+        /*Hides the restart button and shows the 'start game' button */
+        restartButton.classList.add('hidden');
+        startButton.classList.remove('hidden')
+
+        startGame();
+        }
 
     startGame();
-    }
+}
+
+play();
